51de705a3c1833eee1ae17c63138c47de41f43ef
Asad Mahmood <asadmhmd1234@gmail.com>
2022-11-15 12:29:19 +0530

    Make header logo link to home page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,17 @@
 import React, { useContext } from "react";
 import "./Header.css";
 import { ThemeContext } from "../context/ThemeContext.jsx";
+import { Link } from "react-router-dom";
 
 function Header() {
   const { theme, toggleTheme } = useContext(ThemeContext);
   return (
     <header>
       <nav className={`nav-${theme}`}>
-        <h3>Where in the world!</h3>
-        <span onClick={toggleTheme}>
+        <Link to={"/rest-countries/"}>
+          <h3>Where in the world!</h3>
+        </Link>
+        <span className={"theme-toggle"} onClick={toggleTheme}>
           <i
             className={`fa-solid fa-${theme === "light" ? "moon" : "sun"}`}
           ></i>
